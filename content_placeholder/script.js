04d9ef35007f892ca75d086592a8450b8b9275eb
Rytoff <roman.rytoff@gmail.com>
2021-12-03 21:29:26 +0200

    project content placeholder finished

--- content_placeholder/script.js
+++ content_placeholder/script.js
@@ -1,3 +1,6 @@
+// window.addEventListener('load', getData)
+// window.addEventListener('DOMContentLoaded', getData)
+
 const header = document.getElementById('header')
 const title = document.getElementById('title')
 const excerpt = document.getElementById('excerpt')
@@ -8,16 +11,18 @@ const date = document.getElementById('date')
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
-setTimeout(getData, 2500)
+setTimeout(getData, 3500)
 
 function getData() {
   header.innerHTML =
     '<img src="https://source.unsplash.com/random/450x250" alt="" />'
   title.innerHTML = 'Lorem ipsum dolor sit amet'
-  excerpt.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, ipsa.'
-  profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  excerpt.innerHTML =
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, ipsa.'
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
   userName.innerHTML = 'John Smith'
   date.innerHTML = 'Dec 03, 2021'
-  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
-  animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
 }
